7e2f9ce893ffa0146598850f7d37fe6950dfca1f
Mahendra <bus.mahendrat@gmail.com>
2022-04-10 03:42:18 +0200

    added visibility btn to toggle work schedule

--- src/components/TimeShift.jsx
+++ src/components/TimeShift.jsx
@@ -1,18 +1,29 @@
 import { useEffect, useState } from "react";
 
 function TimeShift ({data}) {
-  const [date, setsDate] = useState(null);
+  const [required, setRequired] = useState({
+    day: "",
+    todayDate: "",
+    month: "",
+    year: "",
+  });
 
   useEffect(() => {
-    setsDate(data.toString())
+    let newDate = data.toString().split(" ");
+    setRequired({
+          day: newDate[0],
+          todayDate: newDate[2],
+          month: newDate[1],
+          year: newDate[3],
+        })
   }, [data]);
 
-  console.log(data);
+  console.log(required);
 
   return (
     <>
-      <h1>Time</h1>
-      <h1>{date? <h1>{date}</h1> : null}</h1>
+      <h1>Schedule</h1>
+      <h1>{required? <><p>{required.day} {required.todayDate} {required.month} {required.year}</p></> : null}</h1>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,21 @@ import TimeShift from '../components/TimeShift';
 function Home() {
 
   const [value, setValue] = useState(new Date());
+  const [visible, setVisible] = useState(false);
 
   function onChange(nextValue) {
     setValue(nextValue);
+  }
+
+  const handleVisible = () => {
+    setVisible((visible) => !visible)
   }
 
   return (
     <>
       <h1>Pro Shift</h1>
       <Calendar onChange={onChange} value={value} />
-      <TimeShift data={value}/>
+      {visible ?<> <button onClick={handleVisible}>Hide work schedule</button> <TimeShift data={value}/> </>  : <button onClick={handleVisible}>show work Schedule</button>}
     </>
   )
 
